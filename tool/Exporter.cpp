cde99efbfddb31a28398352ba73fd935a12dad26
Eanigmal <boncoi19990@gmail.com>
2025-05-18 23:20:14 +0700

    add Exporter, load reflection from spirv

---
+++ tool/Exporter.cpp
@@ -0,0 +1,203 @@
+#include <cstdint>
+#include <stdio.h>
+#include <map>
+#include "unified1/spirv.h"
+
+#include "Exporter.h"
+
+int main(int argc, char** argv) {
+	assert(argc == 3);
+	const char* shaderPath = argv[1];
+	printf("input shader path: %s\n", shaderPath);
+	FILE *file = fopen(shaderPath, "r");
+	uint32_t spvBlob[2048];
+	uint32_t spvSize = fread(spvBlob, sizeof(uint32_t), 2048, file);
+	Reflection ref = retrieveReflection(spvBlob, spvSize);
+	fclose(file);
+	printReflection(ref);
+
+	return 0;
+}
+
+Reflection retrieveReflection(const uint32_t* spvBlob, uint32_t spvSize) {
+	Reflection reflection;
+	std::map<uint32_t, std::string> names;
+	std::map<uint32_t, Primitive> types;
+	std::map<uint32_t, uint32_t> pointerToType;
+
+	uint32_t w = 0;
+	while(w < spvSize){
+		if(w == 0) {
+			assert(spvBlob[w] == SpvMagicNumber);
+			++w;
+			continue;
+		}
+		if(w == 1) {
+			// version is word with format
+			// 0x 00-major-minor-00
+			uint32_t version = spvBlob[w];
+			uint32_t minor = version & 0x0000FF00; 
+			minor >>= 8;
+			uint32_t major = version & 0x00FF0000; 
+			major >>= 16;
+			printf("Spirv version: %i.%i\n", major, minor);
+			++w;
+			continue;
+		}
+		if(w >= 2 && w <= 4) {
+			++w;
+			continue;
+		}
+
+		uint16_t opCode = ((uint16_t*)spvBlob)[w * 2];
+		uint16_t wordCount = ((uint16_t*)spvBlob)[w * 2 + 1];
+		switch(opCode) {
+			case SpvOpName: {
+				std::string name;
+				for (size_t i = (w+2)*4, n = (w + wordCount)*4; i < n; i++) {
+					char ch = ((char*)spvBlob)[i];
+					if(ch == '\0') {
+						break;
+					}
+					name.append(1, ch);
+				}
+				// printf("OpName with id: %i, named: %s\n", spvBlob[w+1], name.data());
+				names.emplace(spvBlob[w+1], name);
+				break;
+			} 
+			case SpvOpMemberName:
+				break;
+			case SpvOpDecorate: {
+				// printf("OpDecorate with %i word, id: %i, decoration %i, value %i\n", wordCount, spvBlob[w+1], spvBlob[w+2], spvBlob[w+3]);
+				uint32_t decorate = spvBlob[w+2];
+				if (decorate == SpvDecorationLocation) {
+					Location& l = reflection.locations[reflection.locationCount];
+					l.id = spvBlob[w+1];
+					l.location = (uint8_t)spvBlob[w+3];
+					l.name = names[l.id];
+					reflection.locationCount++;
+				}
+				else if (decorate == SpvDecorationBinding || decorate == SpvDecorationDescriptorSet) {
+					bool first = true;
+					uint32_t id = spvBlob[w+1];
+					unsigned int it = 0;
+					for(; it < reflection.bindingCount; it++) {
+						if (reflection.bindings[it].id == id) {
+							first = false;
+							break;
+						}
+					}
+					Binding& b = reflection.bindings[it];
+					if (first) {
+						b.id = id;
+						// can do this because All OpName ops are called before all OpDecorate ops
+						b.name = names[b.id];
+						reflection.bindingCount++;
+					}
+
+					if (decorate == SpvDecorationBinding)
+						b.binding = (uint8_t)spvBlob[w+3];
+					else if (decorate == SpvDecorationDescriptorSet){
+						b.set = (uint8_t)spvBlob[w+3];
+						reflection.descriptorSetCount = b.set > reflection.descriptorSetCount ? b.set : reflection.descriptorSetCount;
+					}
+
+				}
+				else if (decorate == SpvDecorationBlock) {
+				}
+
+
+				break;
+			}
+			case SpvOpVariable: {
+				uint32_t storageClass = spvBlob[w+3];
+				uint32_t pointerType = spvBlob[w+1];
+				if (storageClass == SpvStorageClassInput || storageClass == SpvStorageClassOutput) {
+					bool first = true;
+					uint32_t id = spvBlob[w+2];
+					unsigned int it = 0;
+					for(; it < reflection.locationCount; it++) {
+						if (reflection.locations[it].id == id) {
+							first = false;
+							break;
+						}
+					}
+					assert(first == false);
+					if (storageClass == uint32_t(1))
+						reflection.locations[it].isInput = true;
+
+					reflection.locations[it].type = types[pointerToType[pointerType]];
+				}
+				else if (storageClass == SpvStorageClassUniformConstant || storageClass == SpvStorageClassUniform){ // for binding
+					bool first = true;
+					uint32_t id = spvBlob[w+2];
+					unsigned int it = 0;
+					for(; it < reflection.bindingCount; it++) {
+						if (reflection.bindings[it].id == id) {
+							first = false;
+							break;
+						}
+					}
+					assert(first == false);
+					if (storageClass == SpvStorageClassUniformConstant)
+						reflection.bindings[it].type = Descriptor::SAMPLER;
+					else if (storageClass == SpvStorageClassUniform)
+						reflection.bindings[it].type = Descriptor::UNIFORM;
+					// push constant don't have decoration for it
+					// else if (storageClass == uint32_t(9))
+					// 	reflection.bindings[it].type = Descriptor::PUSH_CONSTANT;
+				}
+
+				break;
+			}
+			case SpvOpTypePointer: {
+				// the type for the pointer allready declare for this type
+				uint32_t storageClass = spvBlob[w+2];
+				if (storageClass == SpvStorageClassInput || SpvStorageClassOutput)
+					pointerToType.emplace(spvBlob[w+1], spvBlob[w+3]);
+				break;
+			}
+			case SpvOpTypeStruct: {
+				break;
+			}
+			case SpvOpTypeVector: {
+				if (spvBlob[w+3] == 2)
+					types.emplace(spvBlob[w+1], Primitive::F2);
+				else if (spvBlob[w+3] == 3)
+					types.emplace(spvBlob[w+1], Primitive::F3);
+				else if (spvBlob[w+3] == 4)
+					types.emplace(spvBlob[w+1], Primitive::F4);
+
+				break;
+			}
+			case SpvOpTypeMatrix: {
+				break;
+			}
+			case SpvOpTypeSampledImage: {
+				break;
+			}
+		}
+
+		w += wordCount;
+	}
+	// count = max + 1
+	reflection.descriptorSetCount += 1;
+
+	return reflection;
+}
+
+void printReflection(const Reflection& reflection) {
+	printf("Descriptor Set Count %i\n", reflection.descriptorSetCount);
+
+	printf("\nLocation Count %i\n", reflection.locationCount);
+	for(unsigned int i = 0; i < reflection.locationCount; i++) {
+		const Location& loc = reflection.locations[i];
+		printf("At location %i of %s, named %s, type %i (id %i)\n", loc.location, loc.isInput == 1 ? "input" : "output", loc.name.c_str(), loc.type, loc.id);
+	}
+
+	printf("\nBinding Count %i\n", reflection.bindingCount);
+	for(unsigned int i = 0; i < reflection.bindingCount; i++) {
+		const Binding& bin = reflection.bindings[i];
+		printf("At binding %i of set %i, named %s, type %i (id %i)\n", bin.binding, bin.set, bin.name.c_str(), bin.type, bin.id);
+	}
+}
---
+++ tool/Exporter.h
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <stdint.h>
+#include <string>
+
+#define MAX_DESCRIPTOR_SET 4
+#define MAX_BINDING 5
+#define MAX_LOCALTION 10
+#define MAX_PUSH_CONSTANT 3
+enum class Primitive {
+	I = 0,
+	F,
+	F2,
+	F3,
+	F4,
+	F3x3,
+	F4x4,
+	STRUCT,
+};
+
+enum class Descriptor {
+	UNIFORM,
+	// DYNAMIC_UNIFORM???,
+	// STORAGE???,
+	// PUSH_CONSTANT,
+	SAMPLER
+};
+
+struct Location {
+	uint32_t id;
+	std::string name;
+	Primitive type;
+	uint8_t location;
+	bool isInput;
+};
+
+struct Binding {
+	uint32_t id;
+	std::string name;
+	Descriptor type;
+	uint8_t binding;
+	uint8_t set;
+};
+
+struct PushConstant {
+	uint32_t id;
+	std::string name;
+	Primitive type;
+	uint8_t size;
+};
+
+struct Reflection {
+	uint8_t descriptorSetCount{0};
+	uint8_t locationCount{0};
+	Location locations[MAX_LOCALTION];
+	uint8_t bindingCount{0};
+	Binding bindings[MAX_BINDING];
+	uint8_t pushConstantCount{0};
+	PushConstant pushConstants[MAX_PUSH_CONSTANT];
+};
+
+Reflection retrieveReflection(const uint32_t* spvBlob, uint32_t spvSize);
+void printReflection(const Reflection& reflection);
