0a33b608347cc7d62c4a2dabc2e418b55e4b6825
Quang TLN <quang.tranlenhat@gameloft.com>
2025-05-27 18:38:25 +0700

    add current path to powershell script

--- tool/run.ps1
+++ tool/run.ps1
@@ -1,11 +1,13 @@
 # Set the path to your executable (adjust if needed)
-$exePath = "D:\PP\Misanthrope\tool\build\bin\Debug\Exporter.exe"
+$currentPath = Get-Location
 
-$vertexPath   = "D:\PP\Misanthrope\data\shaders\candles.vert.spv"
-$fragmentPath = "D:\PP\Misanthrope\data\shaders\candles.frag.spv"
+$exePath = Join-Path -Path $currentPath -ChildPath 'build\bin\Debug\Exporter.exe' 
 
-$gltfPath   = "D:\PP\Misanthrope\data\models\candles\scene.gltf"
-$binaryPath = "D:\PP\Misanthrope\data\models\candles\scene.bin"
+$vertexPath = Join-Path -Path $currentPath -ChildPath '..\data\shaders\candles.vert.spv'
+$fragmentPath = Join-Path -Path $currentPath -ChildPath '..\data\shaders\candles.frag.spv' 
+
+$gltfPath = Join-Path -Path $currentPath -ChildPath '..\data\models\candles\scene.gltf'
+$binaryPath = Join-Path -Path $currentPath -ChildPath '..\data\models\candles\scene.bin'
 
 & $exePath $vertexPath $fragmentPath $gltfPath $binaryPath
 # & $exePath $gltfPath $binaryPath
